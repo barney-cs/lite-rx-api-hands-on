3bb2aad341d54aad2f1d8f45c3a57eec48742803
陈晟 <chensheng970127@foxmail.com>
2024-11-18 17:32:37 +0800

    作业2

--- src/main/java/io/pivotal/literx/Part08OtherOperations.java
+++ src/main/java/io/pivotal/literx/Part08OtherOperations.java
@@ -4,7 +4,9 @@ import io.pivotal.literx.domain.User;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Learn how to use various other operators.
@@ -15,51 +17,52 @@ public class Part08OtherOperations {
 
 //========================================================================================
 
-	// TODO Create a Flux of user from Flux of username, firstname and lastname.
-	Flux<User> userFluxFromStringFlux(Flux<String> usernameFlux, Flux<String> firstnameFlux, Flux<String> lastnameFlux) {
-		return null;
-	}
+    // TODO Create a Flux of user from Flux of username, firstname and lastname.
+    Flux<User> userFluxFromStringFlux(Flux<String> usernameFlux, Flux<String> firstnameFlux, Flux<String> lastnameFlux) {
+        return Flux.zip(usernameFlux, firstnameFlux, lastnameFlux)
+                .map(t -> new User(t.getT1(), t.getT2(), t.getT3()));
+    }
 
 //========================================================================================
 
-	// TODO Return the mono which returns its value faster
-	Mono<User> useFastestMono(Mono<User> mono1, Mono<User> mono2) {
-		return null;
-	}
+    // TODO Return the mono which returns its value faster
+    Mono<User> useFastestMono(Mono<User> mono1, Mono<User> mono2) {
+        return Mono.firstWithValue(mono1, mono2);
+    }
 
 //========================================================================================
 
-	// TODO Return the flux which returns the first value faster
-	Flux<User> useFastestFlux(Flux<User> flux1, Flux<User> flux2) {
-		return null;
-	}
+    // TODO Return the flux which returns the first value faster
+    Flux<User> useFastestFlux(Flux<User> flux1, Flux<User> flux2) {
+        return Flux.firstWithSignal(flux1, flux2);
+    }
 
 //========================================================================================
 
-	// TODO Convert the input Flux<User> to a Mono<Void> that represents the complete signal of the flux
-	Mono<Void> fluxCompletion(Flux<User> flux) {
-		return null;
-	}
+    // TODO Convert the input Flux<User> to a Mono<Void> that represents the complete signal of the flux
+    Mono<Void> fluxCompletion(Flux<User> flux) {
+        return flux.then();
+    }
 
 //========================================================================================
 
-	// TODO Return a valid Mono of user for null input and non null input user (hint: Reactive Streams do not accept null values)
-	Mono<User> nullAwareUserToMono(User user) {
-		return null;
-	}
+    // TODO Return a valid Mono of user for null input and non null input user (hint: Reactive Streams do not accept null values)
+    Mono<User> nullAwareUserToMono(User user) {
+        return Mono.justOrEmpty(user);
+    }
 
 //========================================================================================
 
-	// TODO Return the same mono passed as input parameter, expect that it will emit User.SKYLER when empty
-	Mono<User> emptyToSkyler(Mono<User> mono) {
-		return null;
-	}
+    // TODO Return the same mono passed as input parameter, expect that it will emit User.SKYLER when empty
+    Mono<User> emptyToSkyler(Mono<User> mono) {
+        return mono.defaultIfEmpty(User.SKYLER);
+    }
 
 //========================================================================================
 
-	// TODO Convert the input Flux<User> to a Mono<List<User>> containing list of collected flux values
-	Mono<List<User>> fluxCollection(Flux<User> flux) {
-		return null;
-	}
+    // TODO Convert the input Flux<User> to a Mono<List<User>> containing list of collected flux values
+    Mono<List<User>> fluxCollection(Flux<User> flux) {
+        return flux.collectList();
+    }
 
 }
